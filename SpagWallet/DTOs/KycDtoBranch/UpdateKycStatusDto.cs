8f3842909aebed02372f24f601fd5183955e970f
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-03-27 20:04:54 +0100

    deleted spagwallet duplicate folder

--- SpagWallet/DTOs/KycDtoBranch/UpdateKycStatusDto.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace SpagWallet.Application.DTOs.KycDtoBranch
-{
-    public class UpdateKycStatusDto
-    {
-        public Guid UserId { get; set; }
-        public bool IsVerified { get; set; }
-    }
-}
